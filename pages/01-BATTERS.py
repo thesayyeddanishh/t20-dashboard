35cd9da92ed052714c6b32f321a0c12576cfc93a
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-05-05 18:32:36 +0530

    Update 01-BATTERS.py

--- pages/01-BATTERS.py
+++ pages/01-BATTERS.py
@@ -773,7 +773,7 @@ def calculate_scoring_angle(area):
 
 # --- Main Combined Function (Chart 6) ---
 def create_wagon_wheel(df_in, delivery_type):
-    FIG_WIDTH = 5
+    FIG_WIDTH = 11
     FIG_HEIGHT = 5
     FIG_SIZE = (FIG_WIDTH, FIG_HEIGHT)
 
