837df3f7f37c5d43dab877e62a55102055581a5f
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-05-05 22:23:51 +0530

    Update 01-BATTERS.py

--- pages/01-BATTERS.py
+++ pages/01-BATTERS.py
@@ -758,8 +758,7 @@ border_rect = patches.Rectangle(
         clip_on=False,
         zorder=0 # Ensure it stays behind any data points if they overlap
     )
-
-fig.patches.append(border_rect)
+    fig.patches.append(border_rect)
     return fig
     
 
