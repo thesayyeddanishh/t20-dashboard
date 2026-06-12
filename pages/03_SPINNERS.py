bbefe963cbf12a5442b227854695ee7b60a5a314
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-06-12 14:47:36 +0530

    Update 03_SPINNERS.py

--- pages/03_SPINNERS.py
+++ pages/03_SPINNERS.py
@@ -363,7 +363,7 @@ def create_Spinner_crease_beehive(df_in, handedness_label): # Renamed function a
     eco_max = eco_values.max() if eco_values.max() > 0 else 18 # Use a suitable default max
     norm = mcolors.Normalize(vmin=0, vmax=eco_max) 
     # cmap = plt.get_cmap('Wistia')
-    cmap = colormaps['Wistia']
+    cmap = colormaps['Wistia'] 
 
     for index, row in summary.iterrows():
         eco = row["Economy"]
