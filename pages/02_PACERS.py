de287e269cd75c7051dae67143285d6f45e69a65
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-06-12 14:58:06 +0530

    Update 02_PACERS.py

--- pages/02_PACERS.py
+++ pages/02_PACERS.py
@@ -353,9 +353,9 @@ def create_pacer_crease_beehive(df_in, handedness_label): # Renamed function and
     # Color Normalization
     eco_values = summary["Economy"].dropna()
     norm = mcolors.Normalize(vmin=3, vmax=9) 
-    # cmap = plt.get_cmap('Wistia')
-    from matplotlib import colormaps 
-    cmap = colormaps['Wistia'] 
+    cmap = plt.get_cmap('Wistia')
+    # from matplotlib import colormaps 
+    # cmap = colormaps['Wistia'] 
 
     for index, row in summary.iterrows():
         eco = row["Economy"]
